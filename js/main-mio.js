02bd70d1c8828f1623e6ecdc2364d708485a2a00
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-26 18:40:03 -0300

    .

---
+++ js/main-mio.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector("#carta-a-santa");
+const nombre = formulario.nombre.value;
+const ciudad = formulario.ciudad.value;
+const comportamiento = formulario.comportamiento.value;
+const descripcionRegalo = formulario['descripcion-regalo'].value;
+
+
