a83ce8f1843ea5138e7e0a2f0f1a080a34dbdb17
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-27 00:21:53 -0300

    agrega validaciones

--- js/main-mio.js
+++ js/main-mio.js
@@ -1,3 +1,9 @@
+const formulario = document.querySelector("#carta-a-santa");
+const nombre = formulario.nombre.value;
+const ciudad = formulario.ciudad.value;
+const comportamiento = formulario.comportamiento.value;
+const descripcionRegalo = formulario['descripcion-regalo'].value;
+
 function validarNombre(nombre) {
     if (nombre.length === 0) {
         return "Este campo no debe quedar vacio.";
@@ -8,8 +14,20 @@ function validarNombre(nombre) {
     }
 }
 
-const formulario = document.querySelector("#carta-a-santa");
-const nombre = formulario.nombre.value;
-const ciudad = formulario.ciudad.value;
-const comportamiento = formulario.comportamiento.value;
-const descripcionRegalo = formulario['descripcion-regalo'].value;
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return "Este campo no debe quedar vacio.";
+    } else {
+        return "";
+    }
+}
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return "Este campo no debe quedar vacio.";
+    } else if (descripcionRegalo.length >= 250) {
+        return "Este campo debe tener menos caracteres.";
+    } else {
+        return "";
+    }
+}
--- js/pruebas-mio.js
+++ js/pruebas-mio.js
@@ -7,4 +7,21 @@ function pruebaValidarNombre() {
     );
 }
 
+function pruebaValidarCiudad() {
+    console.assert(validarCiudad("") === "Este campo no debe quedar vacio.",
+        "Validar ciudad no validó que la ciudad sea mayor a 1 caracter.",
+    );
+}
+
+function pruebaValidarDescripcionRegalo() {
+    console.assert(validarDescripcionRegalo("") === "Este campo no debe quedar vacio.",
+        "Validar descripcion regalo no validó que la descripcion sea mayor a 1 caracter.",
+    );
+    console.assert(validarDescripcionRegalo("123456789101112131415161718192021222324252627282930313233343536373839401234567891011121314151617181920212223242526272829303132333435363738394012345678910111213141516171819202122232425262728293031323334353637383940122232425262728293031323334353637383940") === "Este campo debe tener menos caracteres.",
+        "Validar descripcion no validó que la descripcion sea menor a 50 caracteres.",
+    );
+}
+
 pruebaValidarNombre();
+pruebaValidarCiudad();
+pruebaValidarDescripcionRegalo();
