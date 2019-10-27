0813019f5343c1aecf83b8153d29e0470cd4f60a
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-27 00:12:11 -0300

    .

--- js/main-mio.js
+++ js/main-mio.js
@@ -1,11 +1,9 @@
 function validarNombre(nombre) {
-    if (nombre.lenght === 0) {
+    if (nombre.length === 0) {
         return "Este campo no debe quedar vacio.";
-    } 
-    else if (nombre.lenght >= 50) {
+    } else if (nombre.length >= 50) {
         return "Este campo debe tener menos caracteres.";
-    } 
-    else {
+    } else {
         return "";
     }
 }
