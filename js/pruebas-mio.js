49a92ceae3c9db7bf5ad98c826092d4143b8675a
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-10-27 00:59:09 -0300

    .

--- js/pruebas-mio.js
+++ js/pruebas-mio.js
@@ -26,7 +26,7 @@ function pruebaValidarDescripcionRegalo() {
     console.assert(validarDescripcionRegalo("") === "Este campo no debe quedar vacio.",
         "Validar descripcion regalo no validó que la descripcion sea mayor a 1 caracter.",
     );
-    console.assert(validarDescripcionRegalo("123456789101112131415161718192021222324252627282930313233343536373839401234567891011121314151617181920212223242526272829303132333435363738394012345678910111213141516171819202122232425262728293031323334353637383940122232425262728293031323334353637383940") === "Este campo debe tener menos caracteres.",
+    console.assert(validarDescripcionRegalo("12345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910") === "Este campo debe tener menos caracteres.",
         "Validar descripcion no validó que la descripcion sea menor a 50 caracteres.",
     );
     console.assert(validarNombre("/^[A-z0-9]+$/") === "Este campo tiene caracteres inválidos.",
